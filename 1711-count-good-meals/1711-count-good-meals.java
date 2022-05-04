4f83744a0a45658ce9d8e2659cd353e16b9d348e
Pranoy Das <pranoy.mbstu12001@gmail.com>
2022-05-05 02:46:03 +0600

    Time: 318 ms (36.14%), Space: 118.6 MB (39.68%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.java
@@ -0,0 +1,21 @@
+class Solution {
+    int mod = 1000000007;
+    public int countPairs(int[] deliciousness) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = deliciousness.length;
+        long res = 0;
+        for (int num : deliciousness) {
+            int power = 1;
+            for (int i = 0; i < 22; i++) {
+                if (map.containsKey(power - num)) {
+                    res += map.get(power - num);
+                    res %= mod;
+                }
+                power *= 2;
+            }
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        return (int) res;
+    }
+}
