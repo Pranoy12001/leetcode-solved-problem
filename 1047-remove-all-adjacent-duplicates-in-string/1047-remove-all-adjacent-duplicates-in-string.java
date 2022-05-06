cb7db88b0c5c12bbd2d33cda559c8978dfbfe7b2
Pranoy Das <pranoy.mbstu12001@gmail.com>
2022-05-07 01:59:58 +0600

    Time: 62 ms (49.68%), Space: 56.4 MB (22.58%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char ch : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(ch);
+            } else {
+                if (stack.peek() == ch) {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (Character ch : stack) {
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
