db3e4081416754270d3f36c61ce50e80ad00d7c3
Pranoy Das <pranoy.mbstu12001@gmail.com>
2022-05-08 01:28:27 +0600

    Time: 10 ms (84.39%), Space: 61.8 MB (77.58%) - LeetHub

--- 456-132-pattern/456-132-pattern.java
+++ 456-132-pattern/456-132-pattern.java
@@ -6,28 +6,18 @@ class Solution {
             return false;
         }
         
-        TreeMap<Integer, Integer> rightMap = new TreeMap<>();
-        
-        for (int i = 2; i < nums.length; i++) {
-            rightMap.put(nums[i], rightMap.getOrDefault(nums[i], 0) + 1);
-        }
-        
-        int leftValue = nums[0];
-        for (int j = 1; j < nums.length - 1; j++) {
-            if (leftValue < nums[j]) {
-                Integer numK = rightMap.ceilingKey(leftValue + 1);
-                
-                if (Objects.nonNull(numK) && numK < nums[j]) {
-                    return true;
-                }
+        Deque<Integer> stack = new ArrayDeque<>();
+        int k = -1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (k > 0 && nums[k] > nums[i]) {
+                return true;
             }
             
-            leftValue = Math.min(leftValue, nums[j]);
-            rightMap.put(nums[j + 1], rightMap.get(nums[j + 1]) - 1);
-            
-            if (rightMap.get(nums[j + 1]) == 0) {
-                rightMap.remove(nums[j + 1]);
+            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
+                k = stack.pop();
             }
+            
+            stack.push(i);
         }
         
         return false;
